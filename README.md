c9e7a3f3aa51e69fe832a08dba780066a8f44ade
Tyler Lemburg <trlemburg@gmail.com>
2016-04-01 11:17:45 -0500

    DB script, readme updates

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 ** Installation **
 ------------------
 
-OMG this will be like, hard to install.
 1. run `cd vagrant/dev; vagrant up`
 2. `vagrant ssh` in the vagrant/dev folder to go into the VM (INSIDE the computer!)
 3. Install Inkscape on the VM `sudo yum --disablerepo=repoforgeextras install inkscape` 
-4. Get ImageMagick on the VM. Install pecl by `sudo yum install php-pear`
-5. Then `pecl install imagick`
+4. Install ImageMagick on the VM. `yum install -y ImageMagick ImageMagick-devel`
+5. Create a config file for yourself. In the root, `cp config.sample.php config.inc.php` and edit as necessary.
+6. You will need a database. Run the script `create_database.sql`, found in `/db`, on your SQL DB. 
+7. This creates several users, including the approvers. You will likely want to create a user for yourself in the users table with role `admin`.
+8. The server should now be running at `localhost:8009`.
+
+** Usage **
+-----------
+
+The workflow is this:
+
+1. User creates lockup on New Lockup Page. Lockup gets a preview svg and is shown to user.
+2. Approvers/creative or an admin go in to check out the lockup. They approve it.
+3. Once it has approval of both communicator contact (approver) and creative, user can generate the files.
+4. Preview function is now obsolete, so it redirects to the download page.
+
+** Upcoming Features **
+-----------------------
+* Edit lockups before and after approval.
+* Regenerate files
+* Select more than one style at a time.
---
+++ db/create_db.sql
@@ -0,0 +1,96 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.47)
+# Database: lockups
+# Generation Time: 2016-04-01 15:52:43 +0000
+# ************************************************************
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+# Dump of table lockup_files
+# ------------------------------------------------------------
+
+CREATE TABLE `lockup_files` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `lockup_id` int(11) DEFAULT NULL,
+  `data` longblob,
+  `orientation` varchar(255) DEFAULT NULL,
+  `color` varchar(255) DEFAULT NULL,
+  `reverse` tinyint(1) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# Dump of table lockups
+# ------------------------------------------------------------
+
+CREATE TABLE `lockups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `organization` varchar(255) DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `organization_second_line` varchar(255) DEFAULT NULL,
+  `subject_second_line` varchar(255) DEFAULT NULL,
+  `acronym` varchar(255) DEFAULT NULL,
+  `acronym_subject` varchar(255) DEFAULT NULL,
+  `extension_county` varchar(255) DEFAULT NULL,
+  `style` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `preview_svg` text,
+  `vert_preview_svg` text,
+  `status` varchar(255) DEFAULT 'awaiting_approval',
+  `approver_id` int(11) DEFAULT NULL,
+  `file_organization` varchar(255) DEFAULT NULL,
+  `file_organization_acronym` varchar(255) DEFAULT NULL,
+  `file_department` varchar(255) DEFAULT NULL,
+  `file_department_acronym` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# Dump of table users
+# ------------------------------------------------------------
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `organization` varchar(255) DEFAULT NULL,
+  `organization_acronym` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+INSERT INTO `users` (`id`, `username`, `date_created`, `role`, `name`, `organization`, `organization_acronym`)
+VALUES
+  (1, 's-tlembur1', '2016-02-29 19:59:57', 'admin', 'Tyler Lemburg', NULL, NULL),
+  (3, 'jbrown14', '2016-03-17 15:07:40', 'approver', 'Jill Brown', 'College of Agricultural Sciences and Natural Resources', 'CASNR & IANR'),
+  (4, 'mriese3', '2016-03-17 15:07:40', 'approver', 'Michael Riese', 'Extension', 'EXT'),
+  (5, 'kvondrak1', '2016-03-17 15:07:40', 'approver', 'Kerry McCullough-Vondrak', 'College of Architecture', 'ARCH'),
+  (6, 'tpieper1', '2016-03-17 15:07:40', 'approver', 'Terri Pieper', 'College of Arts and Sciences', 'A&S'),
+  (7, 'sirwingish2', '2016-03-17 15:07:40', 'approver', 'Sheri Irwin Gish', 'College of Business Administration', 'CBA'),
+  (8, 'bstauffer1', '2016-03-17 15:07:40', 'approver', 'Brad Stauffer', 'College of Education and Human Sciences', 'CEHS'),
+  (9, 'jengebretson2', '2016-03-17 15:07:40', 'approver', 'JS Engebretson', 'College of Engineering', 'ENG'),
+  (10, 'sroush2', '2016-03-17 15:07:40', 'approver', 'Sue M. Roush', 'College of Journalism and Mass Communication', 'JMC'),
+  (11, 'awolff2', '2016-03-17 15:07:40', 'approver', 'Amber Wolff', 'College of Law', 'LAW'),
+  (12, 'kanderson1', '2016-03-17 15:07:40', 'approver', 'Kathe Anderson', 'Hixson-Lied College of Fine and Performing Arts', 'FPA'),
+  (14, 'srosso1', '2016-03-17 15:07:40', 'approver', 'Shari Rosso', 'University Housing', 'UH'),
+  (15, 'mplioplis2', '2016-03-17 15:07:40', 'creative', 'Marcelo Plioplis', NULL, NULL);

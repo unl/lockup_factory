e88d1f3ff17124be7f688c80c11840420361fa6b
Tyler Lemburg <trlemburg@gmail.com>
2016-07-13 11:39:04 -0500

    SLashes cause problems

--- src/Models/Lockup.php
+++ src/Models/Lockup.php
@@ -60,11 +60,11 @@ class Lockup extends \ActiveRecord\Model {
 	}
 
 	public function getOrganizationFilename() {
-		return str_replace('&', '', str_replace('"', '', str_replace("'", '', str_replace(' ', '_', $this->file_organization . '_' . $this->file_department_acronym))));
+		return str_replace('/', '_', str_replace('&', '', str_replace('"', '', str_replace("'", '', str_replace(' ', '_', $this->file_organization . '_' . $this->file_department_acronym)))));
 	}
 
 	public function getDepartmentFilename() {
-		return str_replace('&', '', str_replace('"', '', str_replace("'", '', str_replace(' ', '_', $this->file_department . '_' .  $this->file_organization_acronym . '_' . $this->file_department_acronym))));
+		return str_replace('/', '_', str_replace('&', '', str_replace('"', '', str_replace("'", '', str_replace(' ', '_', $this->file_department . '_' .  $this->file_organization_acronym . '_' . $this->file_department_acronym)))));
 	}
 
 	public function getFolderName() {

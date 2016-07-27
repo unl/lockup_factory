54a5755442707154c1e960c2f37d733daa863494
Tyler Lemburg <trlemburg@gmail.com>
2016-07-27 11:01:12 -0500

    Add instructions for Pantone conversion

--- src/Views/download_lockup_files.php
+++ src/Views/download_lockup_files.php
@@ -19,6 +19,31 @@
 			</tbody>
 		</table>
 		<br>
+		<table>
+			<thead>
+				<tr>
+					<th><strong>Please Read!</strong> Instructions for Pantone Lockups</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						This software is unable to input the correct Pantone colors into Adobe Illustrator and .eps files. However, we have provided a script
+						for users to be able to make the conversion in Adobe Illustrator. To execute the script, perform the instructions below.
+						<ol>
+						<li>Download the lockup package as .zip per usual. The scripts will be included in this .zip archive. Extract or open the archive if necessary.</li>
+						<li>Open Adobe Illustrator, there is no need to open a file.</li>
+						<li>Click File &gt; Scripts &gt; Other Scripts...  to open a dialog to run scripts.</li>
+						<li>Navigate to your extracted lockup .zip archive. You should see a horiz_convert_pantone.jsx and a vert_convert_pantone.jsx. Select one of these
+						and click Open. The conversion will begin automatically. Don't click while it's working!</li>
+						<li>Run the other script if necessary. Some lockup styles do not include a certain orientation, thus there are two scripts.</li>
+						<li>Your Pantone .ai files (see under PMS186CP folder) should now be created and have the correct Pantone color selected in Adobe Illustrator.</li>
+						</ol>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<br>
     	<table>
 			<thead>
 				<tr>

82fe641418ca1de9ef257d8f7daeb8db850378be
Thomas Neumann <thomasneumann21800@gmail.com>
2023-08-30 16:55:07 -0500

    Update DB entries

---
+++ db/db_update_fonts_2023-08-30.sql
@@ -0,0 +1,5 @@
+UPDATE lockup_templates
+SET svg = REPLACE(svg, 'Tungsten', 'Oswald');
+
+UPDATE lockup_templates
+SET svg = REPLACE(svg, 'Mercury Display A", "Mercury Display B', 'Source-Serif-4');

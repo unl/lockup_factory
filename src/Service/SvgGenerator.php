c3009d53d13cad20f46da0fbf1cda96505214dc0
Thomas Neumann <thomasneumann21800@gmail.com>
2023-09-08 13:52:26 -0500

    Remove easySVG and commented code

--- src/Service/SvgGenerator.php
+++ src/Service/SvgGenerator.php
@@ -2,15 +2,14 @@
 
 namespace App\Service;
 
-use App\Service\EasySVG;
 use App\Entity\LockupTemplates;
 use Symfony\Component\HttpKernel\KernelInterface;
-use Psr\Log\LoggerInterface;
 
 use Doctrine\Persistence\ManagerRegistry;
 
 class SvgGenerator
 {
+    // MAKE SURE TO CHANGE THE EPS-CMYK FIX IN LOCKUPS CONVERTER IF YOU CHANGE COLORS!!!
     const SCARLET = '#d00000';
     const WHITE = '#ffffff';
     const BLACK = '#000000';
@@ -18,24 +17,19 @@ class SvgGenerator
     const FOUR_H_GREEN = '#339966'; #pantone for this is 347U
     const PANTONE_RED = '#cf0a2c'; #pantone 186cp
     const CMYK_RED = '#da1a32 device-cmyk(0.02, 1.00, 0.85, 0.06)';
-
     // MAKE SURE TO CHANGE THE EPS-CMYK FIX IN LOCKUPS CONVERTER IF YOU CHANGE COLORS!!!
 
     private $projectDir;
     private $doctrine;
-    private $logger;
     private $tmpDirectory;
 
-    public function __construct(KernelInterface $appKernel, ManagerRegistry $doctrine, LoggerInterface $logger)
+    public function __construct(KernelInterface $appKernel, ManagerRegistry $doctrine)
     {
         $this->doctrine = $doctrine;
-        $this->logger = $logger;
 
         $this->projectDir = $appKernel->getProjectDir();
 
         $this->tmpDirectory = $this->projectDir . "/public/temp/";
-        $this->logger->info($this->tmpDirectory);
-
         if (!file_exists($this->tmpDirectory)) {
             mkdir($this->tmpDirectory, 0777, true);
         }
@@ -43,7 +37,7 @@ class SvgGenerator
 
     public function createLockup($template, $lockup, $orient = 'h', $style = 'RGB', $rev = false, $preview = false): string
     {
-        // $svg = new EasySVG();
+        // Sets up colors
         $text_main_color = self::SCARLET;
         $text_secondary_color = self::BLACK;
         $n_main_color = self::SCARLET;
@@ -51,6 +45,7 @@ class SvgGenerator
         $clover_color = self::FOUR_H_GREEN;
         $clover_h_color = self::WHITE;
 
+        // Updates the colors depending on style, rev, and template
         switch ($style) {
             case 'RGB':
                 if ($template == 'v_social') {
@@ -131,10 +126,6 @@ class SvgGenerator
                 break;
         }
 
-        if (!isset($clover_h_color)) {
-            $clover_h_color = $n_secondary_color;
-        }
-
         if (($template == "h_ncta" || $template == "v_ncta") && $preview) {
             $text_main_color = self::BLACK;
             $text_secondary_color = self::BLACK;
@@ -142,45 +133,37 @@ class SvgGenerator
             $n_secondary_color = self::WHITE;
         }
 
-        $this->logger->info($template);
-        $this->logger->info($style);
-        $this->logger->info($rev);
-        $this->logger->info($preview);
-
+        // Gets the lockup template SVG and loads a simpleXMLElement
         $lockup_template = $this->doctrine->getRepository(LockupTemplates::class)->findOneBy(array('slug' => $template));
-        $this->logger->info('ORIGINAL TEMPLATE');
-        $this->logger->info($lockup_template->getSVG());
         $svgXML = new \SimpleXMLElement($lockup_template->getSVG());
         $svgXML->registerXPathNamespace('svg', 'http://www.w3.org/2000/svg');
 
+        // If it is a preview add the class to the SVG tag
         if ($preview) {
             $svgXML->addAttribute("class", "preview");
         }
 
-        $this->logger->info('LOCKUP FIELDS');
+        // Loop through all the lockup fields and update the text in there
         foreach ($lockup as $lockup_field) {
+            // Get the lockup field class and text value
             $lockup_field_slug = $lockup_field->getFields()->getSlug();
             $lockup_field_value = $lockup_field->getValue();
 
-            $this->logger->info($lockup_field_slug);
-            $this->logger->info($lockup_field_value);
-
+            // Some field's values are uppercase only
             if ($lockup_field->getFields()->getUppercase() == 1) {
                 $lockup_field_value = strtoupper($lockup_field->getValue());
             }
 
+            // Find all the tspan with that class (tspan hold the text) and put the text in there
             $slug_elements = $svgXML->xpath('//svg:tspan[@class="' . $lockup_field_slug . '"]');
             if (isset($slug_elements) && $slug_elements !== false) {
-                $this->logger->info(count($slug_elements));
                 foreach ($slug_elements as $slug_element) {
                     $slug_element[0] = $lockup_field_value;
-                    $this->logger->info($slug_element->__toString());
                 }
-            } else {
-                $this->logger->info('none');
             }
         }
 
+        // Loop through all the colors and update them
         $colors = array(
             'n_main_color' => $n_main_color,
             'n_secondary_color' => $n_secondary_color,
@@ -189,80 +172,66 @@ class SvgGenerator
             'clover_color' => $clover_color,
             'clover_h_color' => $clover_h_color,
         );
-
-        $this->logger->info('COLORS');
         foreach ($colors as $color_name => $color_value) {
+            // Find the elements who have that color and update their fill value
             $color_elements = $svgXML->xpath('//svg:*[@class="' . $color_name . '"]');
-            $this->logger->info($color_name);
-            $this->logger->info($color_value);
             if (isset($color_elements) && $color_elements !== false) {
-                $this->logger->info(count($color_elements));
                 foreach ($color_elements as $single_color_element) {
                     $single_color_element['fill'] = $color_value;
-                    $this->logger->info($single_color_element['fill']);
                 }
-            } else {
-                $this->logger->info('none');
             }
         }
 
+        // The center line on the extension 4h has a stroke not a fill so that is here separate
         $center_line_elements = $svgXML->xpath('//svg:*[@class="center_line"]');
         if (isset($center_line_elements) && $center_line_elements !== false) {
-            $this->logger->info(count($center_line_elements));
             foreach ($center_line_elements as $single_center_line_element) {
+                // This will always match the secondary text color
                 $single_center_line_element['stroke'] = $text_secondary_color;
-                $this->logger->info($single_color_element['stroke']);
             }
-        } else {
-            $this->logger->info('none');
         }
 
-        $this->logger->info('NEW SVG');
-        $this->logger->info($svgXML->asXML());
-
-
+        // Generate the file names for temporary use
         $textSVGFile = uniqid('text_svg_', true) . '.svg';
         $pathSVGFile = uniqid('path_svg_', true) . '.svg';
-
         $textSVGPath = $this->tmpDirectory . $textSVGFile;
         $pathSVGPath = $this->tmpDirectory . $pathSVGFile;
 
-        $this->logger->info($textSVGPath);
-
+        // Save the SVG to a file for editing purposes
         $textSVGWrite = file_put_contents($textSVGPath, $svgXML->asXML());
 
+        // If we did not save it then die
         if ($textSVGWrite === false) {
             die("Internal error");
         }
 
+        // Check which actions we can use
         exec("inkscape --action-list", $actionList, $actionListCode);
 
-        $this->logger->info($actionListCode);
-        $this->logger->info(implode("", $actionList));
-
+        // If we are missing actions they might be called verbs (This is inkscape version issues)
         if ($actionListCode !== 0
             || strpos(implode("", $actionList), 'select-all') === false
             || strpos(implode("", $actionList), 'page-fit-to-selection') === false
         ){
+            // Select all the text and fit the canvas to size, convert text to paths, export the SVG
             exec("inkscape " . $textSVGPath . " --verb='EditSelectAll;FitCanvasToSelection' --export-text-to-path --export-plain-svg --export-type=svg --export-filename=" . $pathSVGPath, $execOutput, $execCode);
         } else {
+            // Select all the text and fit the canvas to size, convert text to paths, export the SVG
             exec("inkscape " . $textSVGPath . " --actions='select-all;page-fit-to-selection' --export-text-to-path --export-plain-svg --export-type=svg --export-filename=" . $pathSVGPath, $execOutput, $execCode);
         }
 
-        $this->logger->info($execCode);
-
+        // If we run into issues then die
         if ($execCode !== 0) {
-            $this->logger->error(implode(' / ', $execOutput));
             die("Internal error");
         }
 
+        // Get the updated file and delete the temp ones
         $pathSVG = file_get_contents($pathSVGPath);
-
         unlink($textSVGPath);
         unlink($pathSVGPath);
 
+        // Return the text of the newly made SVG(We do this because if anything goes wrong the new file it will cause an error)
         $pathSVGXML = new \SimpleXMLElement($pathSVG);
-        $this->logger->info($pathSVGXML->asXML());
         return $pathSVGXML->asXML();
     }
 }

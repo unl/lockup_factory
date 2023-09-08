fc659d9ab7e598998463815a87b61a84f726b866
Thomas Neumann <thomasneumann21800@gmail.com>
2023-09-08 09:14:59 -0500

    Fixed error with vertical only lockups and removed trailing spaces

--- public/convert_pantone.jsx
+++ public/convert_pantone.jsx
@@ -1,4 +1,4 @@
-#target illustrator  
+#target illustrator
 
 var rootFolder = (new File($.fileName)).parent.absoluteURI;
 
@@ -8,38 +8,57 @@ fileName = fileName.pop();
 fileName = fileName.substring(0, fileName.lastIndexOf("_lockups"));
 fileName = fileName.substring(2);
 
-createFile(rootFolder+"/Nh_2023_"+fileName+"/4c_CMYK/Nh_2023_"+fileName+"_4c_rev.ai", rootFolder+"/Nh_2023_"+fileName+"/PMS186cp/Nh_2023_"+fileName+"_pms186cp_rev.ai");
-createFile(rootFolder+"/Nh_2023_"+fileName+"/4c_CMYK/Nh_2023_"+fileName+"_4c.ai", rootFolder+"/Nh_2023_"+fileName+"/PMS186cp/Nh_2023_"+fileName+"_pms186cp.ai");
+createFile(
+    rootFolder+"/Nh_2023_"+fileName+"/4c_CMYK/Nh_2023_"+fileName+"_4c_rev.ai",
+    rootFolder+"/Nh_2023_"+fileName+"/PMS186cp/Nh_2023_"+fileName+"_pms186cp_rev.ai"
+);
+createFile(
+    rootFolder+"/Nh_2023_"+fileName+"/4c_CMYK/Nh_2023_"+fileName+"_4c.ai",
+    rootFolder+"/Nh_2023_"+fileName+"/PMS186cp/Nh_2023_"+fileName+"_pms186cp.ai"
+);
 
-createFile(rootFolder+"/Nv_2023_"+fileName+"/4c_CMYK/Nv_2023_"+fileName+"_4c_rev.ai", rootFolder+"/Nv_2023_"+fileName+"/PMS186cp/Nv_2023_"+fileName+"_pms186cp_rev.ai");
-createFile(rootFolder+"/Nv_2023_"+fileName+"/4c_CMYK/Nv_2023_"+fileName+"_4c.ai", rootFolder+"/Nv_2023_"+fileName+"/PMS186cp/Nv_2023_"+fileName+"_pms186cp.ai");
+createFile(
+    rootFolder+"/Nv_2023_"+fileName+"/4c_CMYK/Nv_2023_"+fileName+"_4c_rev.ai",
+    rootFolder+"/Nv_2023_"+fileName+"/PMS186cp/Nv_2023_"+fileName+"_pms186cp_rev.ai"
+);
+createFile(
+    rootFolder+"/Nv_2023_"+fileName+"/4c_CMYK/Nv_2023_"+fileName+"_4c.ai",
+    rootFolder+"/Nv_2023_"+fileName+"/PMS186cp/Nv_2023_"+fileName+"_pms186cp.ai"
+);
 
 function createFile(sourceDoc, destDoc){
-        sourceDoc = open(new File(sourceDoc));
-        destDoc = open(new File(destDoc));
-        
+        sourceFile = new File(sourceDoc);
+        destFile = new File(destDoc);
+
+        if (!sourceFile.exists || !destFile.exists) {
+            return;
+        }
+
+        sourceDoc = open(sourceFile);
+        destDoc = open(destFile);
+
         var scarlet = destDoc.swatches.getByName("PANTONE 186 CP");
         var four_h_green = destDoc.swatches.getByName("PANTONE 347 U");
-    
-        sourceDoc.activate(); 
-    
-        sourceDoc.selection = null; 
-        sourceDoc.artboards.setActiveArtboardIndex(0); 
-        sourceDoc.selectObjectsOnActiveArtboard(); 
-        sel = sourceDoc.selection; 
-        moveObjects(sel, destDoc); 
-    
+
+        sourceDoc.activate();
+
+        sourceDoc.selection = null;
+        sourceDoc.artboards.setActiveArtboardIndex(0);
+        sourceDoc.selectObjectsOnActiveArtboard();
+        sel = sourceDoc.selection;
+        moveObjects(sel, destDoc);
+
         var pathItems = destDoc.pathItems;
-    
+
         for (var i = 0; i < pathItems.length; i++) {
             setPantone(pathItems[i], scarlet, four_h_green);
         };
-    
+
         sourceDoc.close(SaveOptions.DONOTSAVECHANGES);
         destDoc.close(SaveOptions.SAVECHANGES);
 }
 
-function setPantone(pathItem, scarlet, four_h_green){    
+function setPantone(pathItem, scarlet, four_h_green){
     if(pathItem.fillColor.typename == "CMYKColor"){
         if(Math.round(pathItem.fillColor.black) == 6 && Math.round(pathItem.fillColor.magenta) == 100 && Math.round(pathItem.fillColor.cyan) == 2 && Math.round(pathItem.fillColor.yellow) == 85){
             pathItem.fillColor = scarlet.color;
@@ -50,8 +69,8 @@ function setPantone(pathItem, scarlet, four_h_green){
     }
 }
 
-function moveObjects(sel, destDoc) {  
-    for (k=0; k<sel.length; k++) {   
-        var newItem = sel[k].duplicate(destDoc.layers[sel[k].layer.name],ElementPlacement.PLACEATEND);  
-    }  
-}  
+function moveObjects(sel, destDoc) {
+    for (k=0; k<sel.length; k++) {
+        var newItem = sel[k].duplicate(destDoc.layers[sel[k].layer.name],ElementPlacement.PLACEATEND);
+    }
+}
